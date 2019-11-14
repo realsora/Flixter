6dae340d20997018e55b9ed65a6cff16648b8b67
realsora <realsora@gmail.com>
2019-11-14 16:42:54 +0000

    lesson 20

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,5 +1,5 @@
 class Instructor::LessonsController < ApplicationController
-  before_action :autheticate_user!
+  before_action :authenticate_user!
   before_action :require_authorized_for_current_section
 
   def new   
