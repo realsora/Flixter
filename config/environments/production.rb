8f31e2f4758f787dca438da191637d510b5b6200
realsora <realsora@gmail.com>
2019-11-25 16:44:57 +0000

    l22

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = {host: 'flixter-sora.herokuapp.com'}
-}
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
