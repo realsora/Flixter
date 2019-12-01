77b01baf53324b41012b903d270c09fb2028af6f
realsora <realsora@gmail.com>
2019-12-01 03:40:13 +0000

    lesson28

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
   def enrolled_in?(course)
     return enrolled_courses.include?(course)
   end
