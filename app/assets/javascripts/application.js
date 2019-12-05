375ac2304602ce72c7c98c268720f180d3ba515b
realsora <realsora@gmail.com>
2019-12-05 04:29:23 +0000

    lesson 31

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery 
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
@@ -17,3 +18,4 @@
 //= require tether
 //= require bootstrap-sprockets
 //= require_tree .
+//= require jquery-ui
